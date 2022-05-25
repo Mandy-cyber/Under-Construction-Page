d9a872ec05368265a8b70adf99b3ab27c203d980
Mandy-cyber <92461362+PeriodPrideJa@users.noreply.github.com>
2022-05-25 14:47:21 -0500

    adding view routes

--- website/views.py
+++ website/views.py
@@ -2,3 +2,27 @@ from flask import Blueprint, render_template, request
 
 views = Blueprint('views', __name__)
 
+
+@views.route('/under-construction')
+def under_construction_message():
+    return render_template("landing.html")
+
+@views.route('/')
+def home():
+    return render_template("home.html")
+
+@views.route('/about-us')
+def about_us():
+    return render_template("about.html")
+
+@views.route('/contact-us')
+def contact_us():
+    return render_template("contact.html")
+
+@views.route('/help-out')
+def help_out():
+    return render_template("help.html")
+
+@views.route('/donate')
+def donate():
+    return render_template("donate.html")
